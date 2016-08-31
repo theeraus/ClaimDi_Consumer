365dbe5e181e65df025875df10aefc90042cb0b0
T@N <theeraus@gmail.com>
2016-08-31 11:50:27 +0700

    Add project files.

---
+++ ClaimDi_Consumer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimDi_Consumer
+{
+    public class Class1
+    {
+    }
+}
